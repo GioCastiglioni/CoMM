efd0920695e711f6099cab2e7566fc99c2d361bc
GioCastiglioni <g.castiglioni@ug.uchile.cl>
2026-08-16 01:55:51 -0400

    Deleted unnecessary projections

--- models/video_resnet.py
+++ models/video_resnet.py
@@ -3,28 +3,17 @@ import torch.nn as nn
 from torchvision.models.video import r3d_18, R3D_18_Weights
 
 class VideoResNetEncoder(nn.Module):
-    def __init__(self, output_dim=512, pretrained=True):
+    def __init__(self, pretrained=True):
         super().__init__()
         weights = R3D_18_Weights.KINETICS400_V1 if pretrained else None
         model = r3d_18(weights=weights)
         
         # Remove the fully connected classification head
         self.feature_extractor = nn.Sequential(*list(model.children())[:-1])
-        
-        # Output dim of R3D_18 before FC is 512.
-        resnet_out_dim = 512
-        
-        if resnet_out_dim != output_dim:
-            self.proj = nn.Linear(resnet_out_dim, output_dim)
-        else:
-            self.proj = nn.Identity()
 
     def forward(self, x):
         # x: (B, C, T, H, W)
         features = self.feature_extractor(x) # (B, 512, 1, 1, 1) usually if pooled
         features = features.flatten(1) # (B, 512)
-        # Sequence format for MMFusion
-        features = features.unsqueeze(1) # (B, 1, 512)
-        
-        features = self.proj(features)
-        return features
+
+        return features.unsqueeze(1)    # (B, 1, 512)
--- models/wav2vec.py
+++ models/wav2vec.py
@@ -3,17 +3,10 @@ import torch.nn as nn
 import torchaudio
 
 class Wav2Vec2Encoder(nn.Module):
-    def __init__(self, output_dim=512, pretrained=True):
+    def __init__(self, pretrained=True):
         super().__init__()
         bundle = torchaudio.pipelines.WAV2VEC2_BASE
         self.encoder = bundle.get_model()
-        
-        # Determine the output dimension of the Wav2Vec2 model
-        # For WAV2VEC2_BASE, it's 768.
-        wav2vec_out_dim = 768
-        
-        # A linear projection to match the desired embedding dimension
-        self.proj = nn.Linear(wav2vec_out_dim, output_dim)
 
     def forward(self, x):
         # x is assumed to be (batch_size, channels, time)
@@ -22,11 +15,7 @@ class Wav2Vec2Encoder(nn.Module):
             # average over channels if more than 1, or just squeeze
             x = x.mean(dim=1)
             
-        features, _ = self.encoder(x)
-        # features is (batch_size, frames, wav2vec_out_dim)
+        features, _ = self.encoder(x)  # (batch_size, frames, wav2vec_out_dim=768)
         
-        # We can either mean pool over time or keep the sequence.
-        # Since MMFusion uses sequence of tokens or CLS, we will pass the sequence along.
-        features = self.proj(features)
         return features
 
