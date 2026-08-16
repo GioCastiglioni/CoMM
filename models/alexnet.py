f0cff2f9a0a79e3283a4c13c0c3e0a21ef65ff99
Giovanni Castiglioni <g.castiglioni@ug.uchile.cl>
2026-08-16 22:31:20 +0200

    Allow alexnet for espectrogram

--- models/alexnet.py
+++ models/alexnet.py
@@ -4,9 +4,14 @@ import torch
 
 class AlexNetEncoder(AlexNet):
     """AlexNet backbone for features representation learning."""
-    def __init__(self, latent_dim: int = 512, dropout: float = 0.5, global_pool: str = "avg"):
+    def __init__(self, latent_dim: int = 512, dropout: float = 0.5, global_pool: str = "avg", in_channels: int = 3):
         assert global_pool in {"avg", ""}
         super().__init__(dropout=dropout)
+        
+        if in_channels != 3:
+            # Replace the first convolution layer to accept different number of channels
+            self.features[0] = nn.Conv2d(in_channels, 64, kernel_size=11, stride=4, padding=2)
+            
         self.classifier = nn.Linear(256 * 6 * 6, latent_dim)
         self.global_pool = global_pool
 
